aa54fc6280bb4b34ecb7b4811c9a304b3ebc2821
EdsonHTJ <edsonsobralense@gmail.com>
2022-11-09 08:36:49 -0300

    add .env

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from discord.ext import commands
 from discord.ext.commands import cog
 from discord.flags import Intents
 import bot
+from dotenv import load_dotenv
+
 
 cogs = [bot]
 
-apikey = open("./APIKEY.txt","r").read()
-     
+apikey = os.getenv("API_KEY")    
 client = commands.Bot(command_prefix=';', Intents= discord.Intents.all)
 
 for i in range(len(cogs)):
